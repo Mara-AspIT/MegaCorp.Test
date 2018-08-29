133daf68760f5e1064675954188c9ac407313b56
Mads Mikkel Rasmussen <mara@aspit.dk>
2018-08-29 12:31:40 +0200

    Added Deparment. DepartmentRepo.

---
+++ MegaCorp.DataAccess/DepartmentRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MegaCorp.Entities;
+
+namespace MegaCorp.DataAccess
+{
+    public class DepartmentRepository: RepositoryBase
+    {
+        public DepartmentRepository() : base(ConnectionStrings.LocalDb)
+        {
+
+        }
+
+        public List<Department> GetAllDepartments()
+        {
+            string sql = SqlQueries.SelectAll + " Departments";
+            List<Department> departments = new List<Department>(0);
+            DataSet resultSet = Execute(sql);
+            return departments;
+        }
+    }
+}
---
+++ MegaCorp.Entities/Department.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCorp.Entities
+{
+    public class Department: EntityBase
+    {
+
+        public Department(int id, List<Employee> employees, string name)
+            : base(id)
+        {
+            Employees = employees;
+            Name = name;
+        }
+
+        public Department(List<Employee> employees, string name)
+            : this(default, employees, name)
+        {
+
+        }
+
+        public virtual List<Employee> Employees
+        {
+            get; set;
+        }
+        public virtual string Name
+        {
+            get; set;
+        }
+    }
+}
